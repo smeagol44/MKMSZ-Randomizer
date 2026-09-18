ca2d5933885f295a0e13cf63825112bb8611a9ea
Smeag <federicoregueira.89@gmail.com>
2026-09-18 05:23:43 -0300

    Export runtime V1 persistence components

--- src/mkmszr/patches/__init__.py
+++ src/mkmszr/patches/__init__.py
@@ -3,10 +3,16 @@
 from .arena import ArenaReservationPatch
 from .native_payload import NativePayloadPatch, NativePayloadSpec
 from .palette import SubZeroPalettePatch
+from .runtime_v1 import (
+    RuntimeV1FirePersistencePatch,
+    runtime_v1_fire_patches,
+)
 
 __all__ = [
     "ArenaReservationPatch",
     "NativePayloadPatch",
     "NativePayloadSpec",
+    "RuntimeV1FirePersistencePatch",
     "SubZeroPalettePatch",
+    "runtime_v1_fire_patches",
 ]
