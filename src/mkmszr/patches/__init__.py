d30a697e14935d87e35189c51f1727a46760d172
Smeag <federicoregueira.89@gmail.com>
2026-09-18 02:19:27 -0300

    Export native payload patch

--- src/mkmszr/patches/__init__.py
+++ src/mkmszr/patches/__init__.py
@@ -1,6 +1,12 @@
 """Composable native ROM patch modules."""
 
 from .arena import ArenaReservationPatch
+from .native_payload import NativePayloadPatch, NativePayloadSpec
 from .palette import SubZeroPalettePatch
 
-__all__ = ["ArenaReservationPatch", "SubZeroPalettePatch"]
+__all__ = [
+    "ArenaReservationPatch",
+    "NativePayloadPatch",
+    "NativePayloadSpec",
+    "SubZeroPalettePatch",
+]
